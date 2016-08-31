45bab6998a7c82d865baeeef8a98e8142b60c6ca
Romain Francois <romain@r-enthusiasts.com>
2016-08-31 17:36:13 +0200

    eleminate submitButton, use actionButton instead

--- shinyApp/server.R
+++ shinyApp/server.R
@@ -8,43 +8,61 @@ library(leaflet)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
+  # the table to display on the data explorer tab. 
+  # the ui is only updated when the submit button for this tab is clicked
+  explorer_table <- reactive({
+    input$explorer_button
+    
+    # isolate to avoide dependency on selection controls
+    isolate({
+      data <- flights
+      if (!identical(input$country,"All countries")) {
+        data <- data %>%
+          filter(country %in% input$country)
+      }
+      if (!identical(input$city, "All cities")) {
+        data <- data %>%
+          filter(city %in% input$city)
+      }
+      if (!identical(input$year, "All years")) {
+        data <- data %>%
+          filter(year %in% input$year)
+      }
+      if (!identical(input$direction, "All directions")) {
+        data <- data %>%
+          filter(direction %in% input$direction)
+      }
+      if (!identical(input$selectVar, "All")) {
+        data <-data %>%
+          select_( .dots = input$selectVar)
+      }
+      data  
+    })
+  })
+  
   # Filter data based on selections
-  output$table <- DT::renderDataTable(DT::datatable({
-    data <- flights
-    if (!identical(input$country,"All countries")) {
-      data <- data %>%
-        filter(country %in% input$country)
-    }
-    if (!identical(input$city, "All cities")) {
-      data <- data %>%
-        filter(city %in% input$city)
-    }
-    if (!identical(input$year, "All years")) {
-      data <- data %>%
-        filter(year %in% input$year)
-    }
-    if (!identical(input$direction, "All directions")) {
-      data <- data %>%
-        filter(direction %in% input$direction)
-    }
-    if (!identical(input$selectVar, "All")) {
-      data <-data %>%
-        select_( .dots = input$selectVar)
-    }
-    data
-  }))
+  output$table <- DT::renderDataTable(DT::datatable(explorer_table()))
+
+  # the table to display on the summary tab. 
+  # the ui is only updated when the submit button for this tab is clicked
+  summary_table <- reactive({
+    input$summary_button
+    
+    # using isolate to avoid dependency on this tab controls
+    isolate({
+      data <- flights
+      if(!identical(input$groupV, "None")){
+        data <- data %>%
+          group_by_(.dots = input$groupV) %>%
+          summarise(npassengers = sum(passengers), nflights= sum(flights), capacity=sum(capacity), weight=sum(weight))
+      }
+      data  
+    })
+  })
   
   #Summary table
-  output$tabSummary <- DT::renderDataTable(DT::datatable({
-    data <- flights
-    if(!identical(input$groupV, "None")){
-      data <- data %>%
-        group_by_(.dots = input$groupV) %>%
-        summarise(npassengers = sum(passengers), nflights= sum(flights), capacity=sum(capacity), weight=sum(weight))
-    }
-    data
-  }))
-  
+  output$tabSummary <- DT::renderDataTable(DT::datatable(summary_table()))
+
   #plot
   output$plot <- renderPlot({
     plot(data)
@@ -58,4 +76,19 @@ shinyServer(function(input, output) {
       addCircles(cities$longitude, cities$latitude, popup=paste(cities$city, cities$country, sep=", "))
     m
   })
+  
+  dataInBounds <- reactive({
+    if( is.null(input$map_bounds ) ){
+      "foo"
+    } else {
+      "bar"
+    }
+  })
+  
+  output$coords <- renderText({
+    dataInBounds()
+  })
+  
+  
+  
 })
--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -4,65 +4,62 @@ library(dplyr)
 geocode<-dismo::geocode
 library(budflights)
 library(leaflet)
-#test
-# Define UI for application that draws a histogram
+
 shinyUI(fluidPage(
   includeCSS("www/styles.css"),
   navbarPage("SatRday flights fever",
              
-             tabPanel("Data explorer",
-                      sidebarLayout(
-                        sidebarPanel(
-                          titlePanel("Filter data"),
-                          selectInput("country", label=NULL,
-                                      c("All countries", unique(as.character(flights$country))),selected="All countries", multiple=TRUE),
-                          selectInput("city", label=NULL,
-                                      c("All cities", unique(as.character(flights$city))),selected="All cities", multiple=TRUE),
-                          selectInput("direction", label=NULL,
-                                      c("All directions", unique(as.character(flights$direction))),selected="All directions", multiple=FALSE),
-                          selectInput("year",label=NULL,
-                                      c("All years", unique(as.character(flights$year))),selected="All years", multiple=TRUE),
-                          selectInput("selectVar", label = "Select Variable(s) :", 
-                                      c("All", names(flights)), selected = "All", multiple = TRUE),
-                          submitButton("SUBMIT")
-                        ),
-                        mainPanel(
-                          DT::dataTableOutput("table")
-                        )
-                      )
-             ),
-             tabPanel("Summary",
-                      sidebarLayout(
-                        sidebarPanel(
-                          selectInput("groupV", label = "Select Variable(s) to group by :", 
-                                      c("None", names(flights)), selected = "None", multiple = TRUE),
-                          submitButton("SUBMIT")
-                        ),
-                        mainPanel(
-                          DT::dataTableOutput("tabSummary")
-                        )
-                      )
-                      
-             ),
-             tabPanel("Plot", 
-                      plotOutput("plot")                      
-             ),
-             tabPanel("Interactive map",
-                      div( class = "outer", 
-                           leafletOutput("map", height = "100%"), 
-                           
-                           absolutePanel(
-                             id = "controls", class = "panel panel-default", fixed = TRUE,
-                             draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-                             width = 330, height = "auto",
-                                     
-                             selectInput("groupVmap", label = "Select Variable(s) to group by :", 
-                                         c("None", names(flights)), selected = "None", multiple = TRUE),
-                             submitButton("SUBMIT")    
-                           )
-                      )
-                      
+     tabPanel("Data explorer",
+        sidebarLayout(
+          sidebarPanel(
+            titlePanel("Filter data"),
+            selectInput("country", label=NULL,
+                        c("All countries", unique(as.character(flights$country))),selected="All countries", multiple=TRUE),
+            selectInput("city", label=NULL,
+                        c("All cities", unique(as.character(flights$city))),selected="All cities", multiple=TRUE),
+            selectInput("direction", label=NULL,
+                        c("All directions", unique(as.character(flights$direction))),selected="All directions", multiple=FALSE),
+            selectInput("year",label=NULL,
+                        c("All years", unique(as.character(flights$year))),selected="All years", multiple=TRUE),
+            selectInput("selectVar", label = "Select Variable(s) :", 
+                        c("All", names(flights)), selected = "All", multiple = TRUE),
+            actionButton("explorer_button", label = "submit")
+          ),
+          mainPanel(
+            DT::dataTableOutput("table")
+          )
+        )
+     ),
+     tabPanel("Summary",
+        sidebarLayout(
+          sidebarPanel(
+            selectInput("groupV", label = "Select Variable(s) to group by :", 
+                        c("None", names(flights)), selected = "None", multiple = TRUE),
+            actionButton("summary_button", label = "submit")
+          ),
+          mainPanel(
+            DT::dataTableOutput("tabSummary")
+          )
+        )
+              
+     ),
+     tabPanel("Plot", 
+        plotOutput("plot")                      
+     ),
+     tabPanel("Interactive map",
+        div( class = "outer", 
+             leafletOutput("map", height = "100%"), 
+             
+             absolutePanel(
+               id = "controls", class = "panel panel-default", fixed = TRUE,
+               draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+               width = 330, height = "auto",
+                       
+               textOutput("coords")
              )
+        )
+              
+     )
   )
 )
 )
